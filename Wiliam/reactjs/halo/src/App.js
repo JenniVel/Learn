2898a4869baca0d7c07f2d635066b96dc5d128c3
williamtanuwijaya <will.tw0807@gmail.com>
2023-03-21 22:58:24 +0700

    update

--- Wiliam/reactjs/halo/src/App.js
+++ Wiliam/reactjs/halo/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 0,
+      num1: "",
+      num2: "",
+      operator: ""
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleNumInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleOperator = (event) => {
+    const { value } = event.target;
+    this.setState({ operator: value });
+  }
+
+  handleCalculate = () => {
+    const { num1, num2, operator } = this.state;
+    let result;
+    switch (operator) {
+      case '+':
+        result = parseFloat(num1) + parseFloat(num2);
+        break;
+      case '-':
+        result = parseFloat(num1) - parseFloat(num2);
+        break;
+      case '*':
+        result = parseFloat(num1) * parseFloat(num2);
+        break;
+      case '/':
+        result = parseFloat(num1) / parseFloat(num2);
+        break;
+      default:
+        result = 0;
+    }
+    this.setState({ result });
+  }
+
+  render() {
+    const { result, num1, num2 } = this.state;
+    return (
+      <div>
+        <h1>React Calculator</h1>
+        <input type="text" name="num1" value={num1} onChange={this.handleNumInput} />
+        <select name="operator" onChange={this.handleOperator}>
+          <option value="">Select an operator</option>
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="*">*</option>
+          <option value="/">/</option>
+        </select>
+        <input type="text" name="num2" value={num2} onChange={this.handleNumInput} />
+        <button onClick={this.handleCalculate}>=</button>
+        <span>{result}</span>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Calculator;
