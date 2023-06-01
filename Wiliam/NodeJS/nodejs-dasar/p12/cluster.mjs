40a38331cb82e1331046c8a41c7a909c38bec579
williamtanuwijaya <will.tw0807@gmail.com>
2023-06-01 19:41:31 +0700

    update

---
+++ Wiliam/NodeJS/nodejs-dasar/p12/cluster.mjs
@@ -0,0 +1,23 @@
+import cluster from 'cluster';
+import http, { request } from 'http';
+import os from 'os';
+import process from 'process';
+
+if(cluster.isPrimary){
+  for(let i =0; i<os.cpus().length; i++){
+    cluster.fork();
+  }
+  cluster.addListener('exit', (worker)=>{
+    console.info(`Worker ${worker.id} is exited`);
+    cluster.fork();
+  })
+}
+if(cluster.isWorker){
+  const server = http.createServer((request, response)=>{
+    response.write(`Response from process ${process.id}`);
+    response.end();
+    process.exit();
+  })
+  server.listen(3000);
+  console.info(`Start cluster worker ${process.id}`);
+}
