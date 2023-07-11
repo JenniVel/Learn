6b41b6fa88acacd5a281569a6310c1054f317051
williamtanuwijaya <will.tw0807@gmail.com>
2023-07-11 20:18:24 +0700

    belajar NPM

---
+++ Wiliam/NodeJS/NPM/p6/dependency.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+const source = 'JENNIFER VELENSIA SANTOTI';
+const target = _.capitalize(source);
+
+console.info(target);
---
+++ Wiliam/NodeJS/NPM/p6/tutor/index.js
@@ -0,0 +1,10 @@
+import { sayHello, sum } from 'belajarlibrarynodejswilliamtanuwijaya';
+
+import { min, max } from 'belajarlibrarynodejswilliamtanuwijaya/number';
+
+console.info(min(9, 0));
+console.info(max(9, 0));
+
+console.info(sayHello('VELEN'));
+
+console.info(sum([9, 9, 9]));
